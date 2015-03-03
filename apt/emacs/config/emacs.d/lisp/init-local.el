bd52dfc0b8541ee7dc2640b7caaf91601bf7d29b
Rohde Fischer <rohdef@rohdef.dk>
2015-03-03 10:03:12 +0100

    Initial emacs config

---
+++ apt/emacs/config/emacs.d/lisp/init-local.el
@@ -0,0 +1,12 @@
+;;; package -- Summery
+;;; Commentary:
+
+;;; Code:
+(global-linum-mode 1)
+(desktop-save-mode 0)
+
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+(provide 'init-local)
+;;; init-local.el ends here
---
+++ apt/emacs/postinstall.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+
+begin
+  %x[git clone https://github.com/purcell/emacs.d.git ~/.emacs.d]
+end
