8d5890040a4bf28a1262aefb3e0bf39a5b06a03a
Rohde Fischer <rohdef@rohdef.dk>
2015-03-05 10:25:14 +0100

    Updated emacs config and fixed node setup

--- apt/emacs/config/emacs.d/lisp/init-local.el
+++ apt/emacs/config/emacs.d/lisp/init-local.el
@@ -2,11 +2,16 @@
 ;;; Commentary:
 
 ;;; Code:
+;; Normal settings
 (global-linum-mode 1)
 (desktop-save-mode 0)
 
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+;; Spark Core
+(setq auto-mode-alist
+      (append '(("\\.h$" . c++-mode) ("\\.cpp$" . c++-mode) ("\\.ino$" . c++-mode)) auto-mode-alist))
 
 (provide 'init-local)
 ;;; init-local.el ends here
