39801b1894daea87d40f822a206b4a7fb5701e6f
Rohde Fischer <rohdef@rohdef.dk>
2015-03-05 10:44:44 +0100

    24 hour time added to emacs

--- apt/emacs/config/emacs.d/lisp/init-local.el
+++ apt/emacs/config/emacs.d/lisp/init-local.el
@@ -5,7 +5,10 @@
 ;; Normal settings
 (global-linum-mode 1)
 (desktop-save-mode 0)
+(setq display-time-24hr-format t)
+(display-time-mode 1)
 
+;; Full screen
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
